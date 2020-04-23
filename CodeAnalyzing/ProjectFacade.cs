8fdbe81b2d70f9fa0dbda7de7c02c6d2f22fe7e1
dubovtsev <dubovtsev@skbkontur.ru>
2020-04-24 00:07:07 +0400

    + ProjectFacade

---
+++ CodeAnalyzing/ProjectFacade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace CodeAnalyzing
+{
+    public class ProjectFacade
+    {
+        private readonly Project _project;
+
+        public ProjectFacade(Project project)
+        {
+            _project = project;
+        }
+
+        public Document[] GetDocuments(Func<Document, bool> filter = null)
+        {
+            return _project.Documents.Where(x => filter?.Invoke(x) ?? true).ToArray();
+        }
+    }
+}
---
+++ CodeAnalyzingTests/ProjectFacadeTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using CodeAnalyzing;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+
+namespace CodeAnalyzingTests
+{
+    [TestFixture]
+    public class ProjectFacadeTest
+    {
+        [Test]
+        public void GetDocumentsTest()
+        {
+            var solutionFacade = new SolutionFacade(@"C:\dev\diploma\CodeAnalyzing\Tests\TestSolutionFramework.sln");
+            var projects = solutionFacade.GetProjects();
+
+            projects.Length.Should().Be(1);
+            var project = projects.First();
+            project.Name.Should().BeEquivalentTo("TestFrameworkProject");
+        }
+
+        [Test]
+        public void GetDocumentsWithFilterTest()
+        {
+            var solutionFacade = new SolutionFacade(@"C:\dev\diploma\CodeAnalyzing\Tests\TestSolutionFramework.sln");
+            var projects = solutionFacade.GetProjects(x => x.Name != "TestFrameworkProject");
+
+            projects.Length.Should().Be(0);
+        }
+    }
+}
