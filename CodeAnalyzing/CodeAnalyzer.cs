dfb92df4dbab5796a0f0631a5ed13530cd27edbe
dubovtsev <dubovtsev@skbkontur.ru>
2020-04-23 19:55:06 +0400

    + library project

---
+++ CodeAnalyzing/CodeAnalyzer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = System.Console;
+
+namespace CodeAnalyzing
+{
+    public class CodeAnalyzer
+    {
+        public CodeAnalyzer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
